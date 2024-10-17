97e0e3efbd62ac517da57faca4ebd559a4c03960
victor <victorayomide32@gmail.com>
2024-10-17 21:46:08 +0100

    adjustment

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,43 +3,41 @@ import styles from "../styles/Home.module.css";
 import { Header } from "@/components/header/Header";
 import { Summary } from "@/components/summary/Summary";
 import {
-  Designation,
-  Photo,
-  ShortIntro,
+	Designation,
+	Photo,
+	ShortIntro,
 } from "@/components/summary/profile/Profile";
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Victor Olorunfemi - Frontend Engineer</title>
-        <meta
-          name="description"
-          content="Victor Olorunfemi is a Software Engineer with experience in Frontend Engineering. He is a user centered Frontend Engineer who is comfortable working
+	return (
+		<>
+			<Head>
+				<title>Victor Olorunfemi - Fullstack Software Engineer</title>
+				<meta
+					name="description"
+					content="Victor Olorunfemi is a Software Engineer with experience in Fullstack Software Engineering. He is a user centered Fullstack Software Engineer who is comfortable working
 with JavaScript, React.js, and other various frontend web technologies to deliver and transform business ideas into an exceptional
 scalable, accessible, responsive, and user friendly full stack web application following the Test Driven Development (TDD) and
-Component Driven Development (CDD) Methodologies. Victor Olorunfemi is looking to levera Software Engineer with experience in Frontend Engineering. A user centered frontend Engineer who is comfortable working
+Component Driven Development (CDD) Methodologies. Victor Olorunfemi is looking to levera Software Engineer with experience in Fullstack Software Engineering. A user centered Fullstack Software Engineer who is comfortable working
 with JavaScript, React.js, and other various frontend web technologies to deliver and transform business ideas into an exceptional
 scalable, accessible, responsive, and user friendly full stack web application following the Test Driven Development (TDD) and
-Component Driven Development (CDD) Methodologies. I am looking to leverage my experience to solve interesting problems that
-delight end-usersge is experience to solve interesting problems that
-delight end-users"
-        />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=yes"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <Header />
+Component Driven Development (CDD) Methodologies. I am looking to leverage my experience to solve interesting problems that delight end-users"
+				/>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1, shrink-to-fit=yes"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main>
+				<Header />
 
-        <Summary
-          content_one={<Designation />}
-          content_three={<ShortIntro />}
-          content_two={<Photo />}
-        />
-      </main>
-    </>
-  );
+				<Summary
+					content_one={<Designation />}
+					content_three={<ShortIntro />}
+					content_two={<Photo />}
+				/>
+			</main>
+		</>
+	);
 }
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -5,57 +5,59 @@ import { Portfolio } from "@/components/portfolio/Portfolio";
 import styles from "../../styles/Portfolio.module.css";
 
 const index = () => {
-  return (
-    <>
-      <Head>
-        <title>Victor Olorunfemi - Frontend Engineer:::Portfolio </title>
-        <meta
-          name="description"
-          content="Portfolio page of Victor Olorunfemi, a Frontend Engineer based in Nigeria"
-        />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=yes"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <Header />
-        <section className={styles.portfolio_container}>
-          <Portfolio
-            desc="vas, money transfer, and utility services"
-            stacks="JAVASCRIPT + REACT"
-            title="Airvend"
-            link={"https://app.airvend.ng/"}
-          />
-           <Portfolio
-            desc="Payment Gateway"
-            stacks="JAVASCRIPT + REACT"
-            title="Airgate"
-            link={"https://airgate.ng/"}
-          />
-          <Portfolio
-            desc="NPM Packages"
-            stacks="TYPESCRIPT + REACT"
-            title="Frontend Tooling"
-            link={"https://www.npmjs.com/settings/clefayomide/packages"}
-          />
-          <Portfolio
-            desc="Spotify clone"
-            stacks="TYPESCRIPT + REACT"
-            title="Not Spotifffyyy"
-            link={"https://not-spotifffyyy.netlify.app/"}
-          />
-          <Portfolio
-            desc="eCommerce web app for thrift sellers and buyers"
-            stacks="JAVASCRIPT + REACT"
-            title="Thrifit"
-            link={"https://dev-thrifit.netlify.app/"}
-          />
-        </section>
-      </main>
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<title>
+					Victor Olorunfemi - Fullstack Software Engineer:::Portfolio{" "}
+				</title>
+				<meta
+					name="description"
+					content="Portfolio page of Victor Olorunfemi, a Fullstack Software Engineer based in Nigeria"
+				/>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1, shrink-to-fit=yes"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main>
+				<Header />
+				<section className={styles.portfolio_container}>
+					<Portfolio
+						desc="vas, money transfer, and utility services"
+						stacks="JAVASCRIPT + REACT"
+						title="Airvend"
+						link={"https://app.airvend.ng/"}
+					/>
+					<Portfolio
+						desc="Payment Gateway"
+						stacks="JAVASCRIPT + REACT"
+						title="Airgate"
+						link={"https://airgate.ng/"}
+					/>
+					<Portfolio
+						desc="Spotify clone"
+						stacks="TYPESCRIPT + REACT"
+						title="Not Spotifffyyy"
+						link={"https://not-spotifffyyy.netlify.app/"}
+					/>
+					<Portfolio
+						desc="eCommerce web app for thrift sellers and buyers"
+						stacks="JAVASCRIPT + REACT"
+						title="Thrifit"
+						link={"https://dev-thrifit.netlify.app/"}
+					/>
+					<Portfolio
+						desc="NPM Packages"
+						stacks="TYPESCRIPT + REACT"
+						title="Frontend Tooling"
+						link={"https://www.npmjs.com/settings/clefayomide/packages"}
+					/>
+				</section>
+			</main>
+		</>
+	);
 };
 
 export default index;
